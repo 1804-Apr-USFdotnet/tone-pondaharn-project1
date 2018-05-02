50f3fc0db5d1804fdabdf8a3106deb472dda9ce2
tonep <tone.pondaharn@protonmail.com>
2018-05-02 13:51:57 -0400

    initial commit

---
+++ Restaurants0/DataAccess/RestuarantCrud.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class RestaurantCrud
+    {
+        public IEnumerable<Restaurant> ShowRestaurants()
+        {
+            using (var db = new RestaurantsDBEntities())
+            {
+                return db.Restaurants.ToList();
+            }
+        }
+
+        public IEnumerable<Review> ShowRestaurantReviews(int restId)
+        {
+            var db = new RestaurantsDBEntities();
+            return db.Reviews.Where(r => r.RestaurantId == restId);
+        }
+
+        public double ShowAverageRating(int restId)
+        {
+            using (var db = new RestaurantsDBEntities())
+            {
+                double RatingAverage = db.Reviews.Where(r => r.RestaurantId == restId).Average(r => r.rating);
+                // INCORRECT: should update restaurant.avgRating
+                return RatingAverage;
+            }
+        }
+
+        public Restaurant FindRestById(int id)
+        {
+            using (var db = new RestaurantsDBEntities())
+            {
+                return db.Restaurants.Where(x => x.ID == id).ToList()[0];
+            }
+        }
+
+        public IEnumerable<Restaurant> FindRestByName(string restName)
+        {
+            var db = new RestaurantsDBEntities();
+            var rest = db.Restaurants.Where(b => b.name.Contains(restName));
+            return rest;
+        }
+    }
+}
---
+++ Restaurants0/DataAccess/SerializerClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using Newtonsoft.Json;
+using System.IO;
+
+
+namespace DataAccess
+{
+
+    [Serializable]
+    partial class Restaurant
+    {
+
+    }
+
+    [Serializable]
+    partial class Reviews { }
+
+    static public class Serializer
+    {
+        static public string SerializeToJSON()
+        {
+            var db = new RestaurantsDBEntities();
+            db.Configuration.LazyLoadingEnabled = false;
+            db.Configuration.ProxyCreationEnabled = false;
+
+            string json = JsonConvert.SerializeObject(db.Restaurants.ToList(), Formatting.Indented,
+                new JsonSerializerSettings()
+                {
+
+                    ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                });
+            return json;
+        }
+
+        static public void JSONToDeserialize(string fp)
+        {
+            var db = new RestaurantsDBEntities();
+            // deserialize JSON directly from a file
+            using (StreamReader file = File.OpenText(fp))
+            {
+                string temp = file.ReadToEnd();
+                JsonSerializer serializer = new JsonSerializer();
+                List<Restaurant> rest = JsonConvert.DeserializeObject<List<Restaurant>>(temp);
+                foreach(var item in rest)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
